4a8e37c4c5e08d0bfbcbbe9be46ecf13e4db329d
Gabriel Demes <gabrieldemes@nyu.edu>
2020-12-09 15:06:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index do |i|
-    puts i
+  array.each_with_index do |i, index|
+    puts "#{index}" + i
   end
 end
